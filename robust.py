7f63849e1e9a92dc9fd1e2aa24a4feef4c780498
Janiszzz <janiszzz@qq.com>
2025-03-23 22:48:11 +0800

    Update robust.py

--- robust.py
+++ robust.py
@@ -1,87 +1,134 @@
 #ToDo：
+'''
+这是收益检测模块，对抗过拟合的最后环节，基于Lucky Factor
+设计上应该与挖掘模块高度隔离，处于pipeline最下游
+输入多个因子收益序列，分析其中一个因子中性化之后的有效程度，输出检验结果、新因子都被哪几个老因子解释
+'''
 #%%Lucky Factors, Harvey and Liu (2021)
 import pandas as pd
 import numpy as np
 import concurrent.futures
 import statsmodels.api as sm
-def create_sample_dataframes():
-    # 创建一个包含时间戳的日期范围
-    dates = pd.date_range(start='2020-01-01', periods=1000, freq='D')
+from sympy.codegen.cfunctions import isnan
 
-    # 假设我们有两个DataFrame，一个包含资产收益率，一个包含因子值
-    asset_returns = pd.DataFrame({
-        'date': dates,
-        'asset_1': np.random.randn(1000),
-        'asset_2': np.random.randn(1000),
-        'asset_3': np.random.randn(1000)
-    }).set_index('date')
 
-    factor_values = pd.DataFrame({
-        'date': dates,
-        'factor_1': np.random.randn(1000),
-        'factor_2': np.random.randn(1000),
-        'factor_3': np.random.randn(1000),
-        'factor_4': np.random.randn(1000),
-        'factor_5': np.random.randn(1000),
-        'factor_6': np.random.randn(1000)
-    }).set_index('date')
-    return asset_returns, factor_values
+class RobustTest(object):
+    def __init__(self, fund, factor):
+        data = pd.concat([fund, factor], axis=1).dropna()
+        X = data.iloc[:-1, 1:].reset_index(drop=True)
+        y = data.iloc[1:, 0].reset_index(drop=True)
 
-def neu(X,y):
-    OX = pd.DataFrame()
-    T = {}
-    #逐个进行单因子回归
-    for i in range(X.shape[1]):
-        Xi = X.iloc[:, i]
-        model = sm.OLS(Xi, sm.add_constant(y)).fit()
-        OX = pd.concat([OX,model.resid],axis=1)
-        
-        model = sm.OLS(y, sm.add_constant(Xi)).fit()
-        T[X.columns[i]] = model.tvalues.iloc[1]
-        
-        
-    OX.columns = X.columns
-    T = pd.DataFrame(T,index = [y.name]).T.abs()
-    return OX,T
+        self.X = X  # 因子数据
+        self.y = y  # 资产收益数据
+        self.OX = pd.DataFrame()  # 正交化后的因子数据
+        self.T = pd.DataFrame()  # t统计量
+        self.fund_name = fund.name
+
+    @staticmethod
+    def create_sample_dataframes():
+        # 创建一个包含时间戳的日期范围
+        dates = pd.date_range(start='2020-01-01', periods=1000, freq='D')
+
+        # 假设我们有两个DataFrame，一个包含资产收益率，一个包含因子值
+        asset_returns = pd.DataFrame({
+            'date': dates,
+            'asset_1': np.random.randn(1000),
+            'asset_2': np.random.randn(1000),
+            'asset_3': np.random.randn(1000)
+        }).set_index('date')
+
+        factor_values = pd.DataFrame({
+            'date': dates,
+            'factor_1': np.random.randn(1000),
+            'factor_2': np.random.randn(1000),
+            'factor_3': np.random.randn(1000),
+            'factor_4': np.random.randn(1000),
+            'factor_5': np.random.randn(1000),
+            'factor_6': np.random.randn(1000)
+        }).set_index('date')
+        return asset_returns, factor_values
+
+    def neu(self):
+        #逐个进行单因子回归
+        self.T = {}
+        for i in range(self.X.shape[1]):
+            Xi = self.X.iloc[:, i]
+            model = sm.OLS(Xi, sm.add_constant(self.y)).fit()
+            self.OX = pd.concat([self.OX,model.resid],axis=1)
+
+            model = sm.OLS(self.y, sm.add_constant(Xi)).fit()
+            self.T[self.X.columns[i]] = model.tvalues.iloc[1]
+
+        self.OX.columns = self.X.columns
+        self.T = pd.DataFrame(self.T,index = [self.y.name]).T.abs()
+        return self.OX,self.T
 
     
-def bootstrap_resample(data):
-    indices = np.random.choice(range(data.shape[0]),  data.shape[0], replace=True)
-    bootstrapped_data = data.iloc[indices,:]     
-    return bootstrapped_data
+    def bootstrap_resample(self, data):
+        indices = np.sort(np.random.choice(range(data.shape[0]),  data.shape[0], replace=True))
+        bootstrapped_data = data.iloc[indices,:]
+        return bootstrapped_data
 
-def max_statistic(data):
-    bs_y = data.iloc[:,0]
-    bs_OX = data.iloc[:,1:]
-    
-    t_statistics = []
-    
-    for i in range(bs_OX.shape[1]):
-        bs_OXi = bs_OX.iloc[:, i]
-        model = sm.OLS(bs_y, sm.add_constant(bs_OXi)).fit()
-        t_statistics.append(model.tvalues.iloc[1])
-        
-    return np.max(t_statistics)
+    def max_statistic(self, data):
+        bs_y = data.iloc[:,0]
+        bs_OX = data.iloc[:,1:]
 
-def bootstrap_once(OX, y, T, n_bootstraps=1000):
-    
-    max_statistic_pdf = []
-    futures = []
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        data = pd.concat([y, OX], axis=1)
-        for i in range(n_bootstraps):
-            future = executor.submit(max_statistic, bootstrap_resample(data))
-            futures.append(future)
-        for future in concurrent.futures.as_completed(futures):
-            max_statistic_pdf.append(future.result())
+        t_statistics = []
 
-    max_statistic_pdf = np.sort(np.abs(np.array(max_statistic_pdf)))
-    modifd_P = T.abs().apply(lambda x : np.searchsorted(max_statistic_pdf,x)/len(max_statistic_pdf))
-    modifd_P = 1- modifd_P
+        for i in range(bs_OX.shape[1]):
+            bs_OXi = bs_OX.iloc[:, i]
+            model = sm.OLS(bs_y, sm.add_constant(bs_OXi)).fit()
+            t_statistics.append(model.tvalues.iloc[1])
 
-    eff_fct_name = modifd_P.loc[modifd_P[y.name]<0.1].index
-    
-    return eff_fct_name,modifd_P,max_statistic_pdf
+        return np.max(t_statistics)
+
+    def bootstrap_once(self, n_bootstraps=1000):
+
+        max_statistic_pdf = []
+        futures = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            data = pd.concat([self.y, self.OX], axis=1)
+            for i in range(n_bootstraps):
+                future = executor.submit(self.max_statistic, self.bootstrap_resample(data))
+                futures.append(future)
+            for future in concurrent.futures.as_completed(futures):
+                max_statistic_pdf.append(future.result())
+
+        max_statistic_pdf = np.sort(np.abs(np.array(max_statistic_pdf)))
+        modifd_P = self.T.abs().apply(lambda x : np.searchsorted(max_statistic_pdf,x)/len(max_statistic_pdf))
+        modifd_P = 1- modifd_P
+
+        eff_fct_name = modifd_P.loc[modifd_P[y.name]<0.1].index
+
+        return eff_fct_name,modifd_P,max_statistic_pdf
+    def work(self):
+        while (1):
+            if (self.X.shape[0] < 100 or self.X.shape[1] == 0):
+                print("out")
+                break
+            self.neu()
+
+            eff_fct_name, modifd_P, max_statistic_pdf = self.bootstrap_once(OX, y, T)
+            not_eff_fct_name = list(set(self.X.columns) - set(eff_fct_name))
+
+            if (len(not_eff_fct_name) == 0 or len(eff_fct_name) == 0):
+                modifd_P.columns = [self.fund_name]
+                #ans = pd.concat([ans, modifd_P], axis=1)
+                #print(modifd_P)
+                return modifd_P
+
+            oy = self.y
+            for i in eff_fct_name:
+                EXi = self.X.loc[:, i]
+                model = sm.OLS(oy, sm.add_constant(EXi)).fit()
+                # print(model.summary())
+                oy = model.resid
+
+            self.X = self.X[not_eff_fct_name]
+            self.y = oy
+        print("Finish!")
+        print()
+
 #%%
 '''
     import init
@@ -101,42 +148,24 @@ fct = pd.read_excel(r".\指数行情序列.xlsx").set_index("date")
 fct = fct.pct_change()
 # ret = fct
 # %%
-ans = pd.DataFrame()
 print("start")
+result = []
 for fund in ret.columns:
     # fund = '005833.OF'
-    data = pd.concat([ret[fund], fct], axis=1).dropna()
-
-    X = data.iloc[:-1, 1:].reset_index(drop=True)
-    y = data.iloc[1:, 0].reset_index(drop=True)
-
-    while (1):
-        if (X.shape[0] < 100 or X.shape[1] == 0):
-            print("out")
-            break
-        OX, T = neu(X, y)
-
-        eff_fct_name, modifd_P, max_statistic_pdf = bootstrap_once(OX, y, T)
-        not_eff_fct_name = list(set(X.columns) - set(eff_fct_name))
-
-        if (len(not_eff_fct_name) == 0 or len(eff_fct_name) == 0):
-            modifd_P.columns = [fund]
-            ans = pd.concat([ans, modifd_P], axis=1)
-            print(modifd_P)
-            break
-
-        oy = y
-        for i in eff_fct_name:
-            EXi = X.loc[:, i]
-            model = sm.OLS(oy, sm.add_constant(EXi)).fit()
-            # print(model.summary())
-            oy = model.resid
-
-        X = X[not_eff_fct_name]
-        y = oy
-
-ans.to_excel("ans.xlsx")
+    new = RobustTest(ret[fund], fct)
+    result.append(new.work())
+result.to_excel("result.xlsx")
 #%%
+import pandas as pd
+import numpy as np
+import concurrent.futures
+import statsmodels.api as sm
+
+fct = pd.read_excel(r".\betaplus-1000-indexdaily.xlsx").set_index("date")
+fct = fct.pct_change()
+ret = pd.read_excel(r".\ret.xlsx").set_index("date")
+
+
 def panel(X,y):
     model = sm.OLS(y, sm.add_constant(X)).fit()
     B = model.params
@@ -149,9 +178,8 @@ def panel(X,y):
 def fake_fund(X,B,OX):
     indices = np.random.choice(range(OX.shape[0]),  OX.shape[0], replace=True)
     bootstrapped_OX = OX.iloc[indices].reset_index(drop=True) 
-    fake_y = X.mul(B.iloc[0,:],axis=1).drop('const',axis=1).sum(axis=1)+bootstrapped_OX
+    fake_y = X.mul(B.iloc[0,:],axis=1).drop('const',axis=1).sum(axis=1) + bootstrapped_OX
     return fake_y
-    
     
 def bootstrap_fake_fund(X,B,OX,T, n_bootstraps=1000):
     max_statistic_pdf = []
@@ -185,6 +213,8 @@ for fund in ret.columns:
 
     X = data.iloc[:-1,1:].reset_index(drop=True)
     y = data.iloc[1:,0].reset_index(drop=True)
+    if(y.shape[0] == 0):
+        continue
     #X = data.iloc[:,1:].reset_index(drop=True)
     #y = data.iloc[:,0].reset_index(drop=True)
 
@@ -193,20 +223,109 @@ for fund in ret.columns:
     modifd_P.index = [fund]
     print(modifd_P)
     ans = pd.concat([ans, modifd_P])
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 
+#%%
+def get_interval(df, start=None, end=None):
+    # 假设 df 的索引是时间序列
+    if isinstance(df.index, pd.DatetimeIndex):
+        if start is None and end is None:
+            return df  # 返回完整数据框，因为没有时间限制
+
+        elif start is not None and end is not None:
+            return df.loc[start:end]
+
+        elif start is not None:
+            # 筛选从 start 到最后一个时间点的数据
+            return df.loc[start:]
+
+        else:  # end is not None
+            # 筛选从第一个时间点到 end 的数据
+            return df.loc[:end]
+
+    # 如果没有时间索引，假设有一个时间列名为 'datetime'
+    elif 'datetime' in df.columns:
+        mask = []
+
+        if start is not None and end is not None:
+            mask = df['datetime'].between(start, end)
+        elif start is not None:
+            mask = df['datetime'] >= start
+        else:  # end is not None
+            mask = df['datetime'] <= end
+
+        return df[mask]
+
+    else:
+        raise ValueError("DataFrame 必须包含时间索引或 'datetime' 列")
+
+def work(fund,fct):
+    data = pd.concat([fund,fct],axis=1).dropna()
+    X = data.iloc[:-1,1:].reset_index(drop=True)
+    y = data.iloc[1:,0].reset_index(drop=True)
+    if(y.shape[0] == 0):
+        return -1
+    #X = data.iloc[:,1:].reset_index(drop=True)
+    #y = data.iloc[:,0].reset_index(drop=True)
+
+    B,OX,T = panel(X, y)
+    modifd_P,max_statistic_pdf = bootstrap_fake_fund(X,B,OX,T)
+
+    return modifd_P
+
+def parse_name_dates(s):
+    """
+    将字符串格式 '姓名(开始日期-结束日期)' 拆解为姓名和两个datetime对象
+
+    Args:
+        s (str): 输入字符串，例如 '盛丰衍(20180711-20250101)'
+
+    Returns:
+        dict: 包含以下键的字典：
+            - 'name': 提取的姓名
+            - 'start_date': 开始日期 datetime 对象
+            - 'end_date': 结束日期 datetime 对象
+
+    Raises:
+        ValueError: 如果字符串格式不正确或无法解析日期
+    """
+    from datetime import datetime
+    try:
+        # 拆分名称和日期部分
+        name_part = s.split('(')[0]
+        date_part = s.split(')')[0].split('(')[1]  # 获取括号内的内容
+
+        # 解析开始和结束日期
+        start_date_str, end_date_str = date_part.split('-')
+        date_format = '%Y%m%d'
+
+        start_date = datetime.strptime(start_date_str, date_format)
+        end_date = datetime.strptime(end_date_str, date_format)
+
+        return {
+            'name': name_part,
+            'start_date': start_date,
+            'end_date': end_date
+        }
+
+    except (IndexError, ValueError) as e:
+        raise ValueError(f"无法解析字符串 '{s}': {e}")
+
+intervals = pd.read_excel(r".\datepairs.xlsx")
+ans = []
+for fund in intervals.columns:
+    for datepairs in intervals[fund]:
+        if(pd.isna(datepairs)):
+            continue
+        tmp = parse_name_dates(datepairs)
+        a,b = get_interval(ret[fund],tmp['start_date'],tmp['end_date']),get_interval(fct,tmp['start_date'],tmp['end_date'])
+        result = work(a.fillna(0),b.fillna(0))
+        print(fund,datepairs,result)
+        ans.append([fund,datepairs,result])
+
+        
+        
+        
+pd.DataFrame(ans).to_excel(r".\250318.xlsx")
 
 
 
