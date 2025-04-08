31a6b2a5e91f2b853c8b3ff038b7eb82e72168c8
Janiszzz <janiszzz@qq.com>
2025-04-08 21:32:54 +0800

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -24,6 +24,7 @@ class BacktestBase(object):
 
         self.get_rebalance_data()
         self.get_position_data()
+        self.get_daily_position_data()
         ''' 
         self.melt_weights()
         self.get_rebalance_data()
@@ -49,39 +50,46 @@ class BacktestBase(object):
         self.cost_price = pd.pivot_table(self.cost_price, values='value', index=['input_ts', 'datetime'], columns=['code'], )
         self.cost_price.columns.name = ""
         self.cost_ret = self.cost_price.pct_change().fillna(0)
-        self.start = self.cost_price['datetime'][0]
-        self.end = self.cost_price['datetime'][-1]
-
+        self.start = self.cost_price.index.get_level_values('datetime')[0]
+        self.end = self.cost_price.index.get_level_values('datetime')[-1]
+        self.weight.index = self.cost_price.index
         return self.cost_price
 
     def get_position_data(self):
         self.amount = self.cost_ret * self.weight
         self.amount['amount'] = (self.amount.sum(axis=1).cumsum() + 1) * self.initial_amount
+        #self.amount.reset_index(inplace=True)
+        #self.amount = self.amount[['datetime','amount']].set_index('datetime')
+        self.amount = self.amount['amount']
         return self.amount
 
     def get_daily_position_data(self):
         
         db = Datafeed("daily_market_data")
         params = {
-            'start_date': self.start,
-            'end_date': self.end,
+            'start_date': str(self.start),
+            'end_date': str(self.end),
             'code': self.weight.columns,
             'metric': "收盘价(元)"
         }
         close_price_ts = db.query_data(params)
-
-        self.initial_amount * self.weight
-        self.cost_price
-
-        return
+        #close_price_ts.set_index("datetime", inplace=True)
+        close_price_ts = pd.pivot_table(close_price_ts, values='value', index=['datetime'], columns=['code'], )
+        close_price_ts.columns.name = ""
+        self.position = self.weight.mul(self.amount, axis=0)
+        self.position = self.position.div(self.cost_price)
+        self.position = self.position.droplevel('input_ts').drop_duplicates()
+        self.position = self.position.reindex(close_price_ts.index, method='ffill')
+        self.daily_amount = self.position.mul(close_price_ts, axis=0).sum(axis=1).astype(float)
+        self.nav = self.daily_amount / self.initial_amount
+        return self.daily_amount
 
 #%%
 if __name__ == '__main__':
     # 虚拟的权重序列
-    weights = pd.DataFrame(0.333, index=pd.date_range(start='2024-01-01 10:00:00', end='2025-01-01 10:00:00', freq='1W'),columns=['000010.SZ','000001.SZ','000002.SZ',])
+    weights = pd.DataFrame(0.33333333, index=pd.date_range(start='2024-01-01 10:00:00', end='2025-01-01 10:00:00', freq='1D'),columns=['000010.SZ','000001.SZ','000002.SZ',])
     weights.index.name = "input_ts"
 
     bb = BacktestBase(weight=weights, symbol="", amount=1000000)
-    bb.amount
     #name = 结算持仓市值（不考虑余额）
 
